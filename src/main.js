6921e435df11383b719d63454dea012f71532e78
nrm176 <nerimplo.tokyo@gmail.com>
2019-04-17 17:15:38 +0900

    initial commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Plotly from 'plotly.js-dist'
 
 Vue.config.productionTip = false
+
+Vue.component('reactive-chart', {
+  props: ['chart'],
+  template: '<div :ref="chart.uuid"></div>',
+  mounted () {
+    Plotly.plot(this.$refs[this.chart.uuid], this.chart.traces, this.chart.layout)
+  },
+  watch: {
+    chart: {
+      handler: function () {
+        Plotly.react(
+          this.$refs[this.chart.uuid],
+          this.chart.traces,
+          this.chart.layout
+        )
+      },
+      deep: true
+    }
+  }
+})
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Chart />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Chart from '../components/Chart'
 
 export default {
   name: 'home',
   components: {
+    Chart,
     HelloWorld
   }
 }
